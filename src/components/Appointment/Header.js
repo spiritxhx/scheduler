045289e745b0585cdda3d4fc3f0db038bd793d63
spiritxhx <spiritxhx@gmail.com>
2019-08-05 18:15:03 -0400

    add Appointment and Header component

---
+++ src/components/Appointment/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header class="appointment__time">
+      <h4 class="text--semi-bold">12pm</h4>
+      <hr class="appointment__separator" />
+    </header>
+  )
+}
--- stories/index.js
+++ stories/index.js
@@ -4,7 +4,6 @@ import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
 import "index.scss";
-
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 
@@ -12,6 +11,8 @@ import DayList from "components/DayList";
 
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
 
 storiesOf("Button", module)
   .addParameters({
@@ -126,3 +127,12 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm" />);
+
+storiesOf("Header", module)
+.add("Header", ()=> <Header />)
