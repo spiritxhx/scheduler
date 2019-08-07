1e5a7de56b1fc6a7f1b2dea542ae55102afe7583
spiritxhx <spiritxhx@gmail.com>
2019-08-07 09:56:13 -0400

    improve the getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -8,9 +8,9 @@ export function getAppointmentsForDay(state, day) {
     }
   }
   for (const appointment in state.appointments) {
-    for (let i=0;i<ans.length;i++){
-      if(ans[i]===state.appointments[appointment].id){
-        ans[i]=state.appointments[appointment];
+    for (let i = 0; i < ans.length; i++) {
+      if (ans[i] === state.appointments[appointment].id) {
+        ans[i] = state.appointments[appointment];
       }
     }
   }
@@ -18,17 +18,13 @@ export function getAppointmentsForDay(state, day) {
 }
 
 export function getInterview(state, interview) {
-  if(!interview){
+  if (!interview) {
     return null;
   }
   let id = interview.interviewer;
   let studentName = interview.student;
-  for (const interviewer in state.interviewers) {
-    if (id === state.interviewers[interviewer].id) {
-      return {
-        interviewer: state.interviewers[interviewer], 
-        student: studentName
-      };
-    }
+  return {
+    interviewer: state.interviewers[String(Object.keys(state.interviewers).find(key => id === Number(key)))],
+    student: studentName
   }
 }
