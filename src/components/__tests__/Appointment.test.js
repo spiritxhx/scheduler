074791bb1b5b4e9c87153c4e243d44afd5b49fe6
spiritxhx <spiritxhx@gmail.com>
2019-08-12 19:47:16 -0400

    fixed the problem by running the backend and frontend server on different enviroments

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -1,24 +1,22 @@
-/*
-  We are rendering `<Application />` down below, so we need React.createElement
-*/
 import React from "react";
 
-/*
-  We import our helper functions from the react-testing-library
-  The render function allows us to render Components
-*/
-import { render } from "@testing-library/react";
+import {
+  render,
+  cleanup,
+  fireEvent,
+  waitForElement
+} from "@testing-library/react";
 
-/*
-  We import the component that we are testing
-*/
-import Application from "components/Application";
+import Application from "../Application";
 
-/*
-  A test that renders a React Component
-*/
+afterEach(cleanup);
 describe("Appointment", () => {
-  it("renders without crashing", () => {
-    render(<Application />);
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday")).then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    });
   });
 });
