786d26cfa126b21def2d641044c1a4c2ca4d387d
spiritxhx <spiritxhx@gmail.com>
2019-08-13 09:19:00 -0400

    test the application, theres a bug of not reading the mock data

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -43,7 +43,7 @@ export default function Appointment(props) {
   }, [props.interview, transition, mode]);
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
 
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,21 @@
 import React from "react";
-
-import { render, cleanup } from "@testing-library/react";
-
+import { render, cleanup, waitForElement } from "@testing-library/react";
 import Application from "components/Application";
+import { getByText, prettyDOM } from "@testing-library/react";
+
+const { getyByText } = render(<Application />);
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+describe("Application", () => {
+  it("renders without crashing", () => {
+    render(<Application />);
+  });
+  it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(()=>{
+      getByText(container, "Archie Cohen")
+    });
+    console.log(prettyDOM(container));
+  });
 });
