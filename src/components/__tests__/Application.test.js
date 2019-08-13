c821cbdfa8908a970a7bb1bca898bc4c28b99d99
spiritxhx <spiritxhx@gmail.com>
2019-08-13 16:59:29 -0400

    create and pass the test for editing

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -91,7 +91,7 @@ describe("Appointment", () => {
 
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
     // 1. Render the Application.
-    const { container } = render(<Application />);
+    const { container,debug } = render(<Application />);
 
     // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
@@ -100,7 +100,23 @@ describe("Appointment", () => {
     // We change the name and save the interview.
     // We don't want the spots to change for "Monday", since this is an edit.
     // Read the errors because sometimes they say that await cannot be outside of an async function.
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(queryByAltText(appointment, "Edit"));
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
+    fireEvent.click(getByText(appointment, "Save"));
+    expect(getByText(appointment, "SAVING")).toBeInTheDocument();
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "3 spots remaining")).toBeInTheDocument();
   });
 });
